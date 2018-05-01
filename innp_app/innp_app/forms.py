64b94ab6601ffa08ccc4be307b619b0109cecd5d
2017-0108-2055\Administrator <tyhg@sina.com>
2018-05-01 08:24:59 +0800

    完成注册功能

--- innp_app/innp_app/forms.py
+++ innp_app/innp_app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
-from wtforms import ()
-from wtforms.validators import ()
+from wtforms import (StringField, PasswordField, SubmitField)
+from wtforms.validators import (DataRequired, EqualTo, Regexp, Length,Email)
+from .models import User
 
 
 class LoginForm(FlaskForm):
@@ -14,4 +15,9 @@ class RegisterForm(FlaskForm):
     """
     注册表单
     """
-    pass
+    email = StringField(_('Email:'), [DataRequired(), Email()])
+    username = StringField('用户名', validators=[DataRequired(), Length(1, 64), Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                                                                    '用户名必须由字母数、字数、下划线或 . 组成')])
+    password = PasswordField('密码', validators=[DataRequired(), EqualTo('password2', message='密码必须一至')])
+    password2 = PasswordField('确认密码', validators=[DataRequired()])
+    submit = SubmitField('马上注册')
