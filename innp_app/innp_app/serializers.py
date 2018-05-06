cc6f33c997449ef81964880bc1c1708c40b88d96
wuyouwei <qnmdgnmd1@163.com>
2018-05-06 22:23:54 +0800

    fix #73 updatascript_v.0.1

--- innp_app/innp_app/serializers.py
+++ innp_app/innp_app/serializers.py
@@ -8,68 +8,102 @@ from marshmallow import (Schema, fields, validate, post_load,
                          validates_schema, ValidationError)
 
 
-class IndexSchema(Schema):
+class CmemberSchema(Schema):
+    """
+    部委
+    author:lyfy
+    :return:Id ， imagePaths，title，insertTime，pubtime，shortContent，source
+    """
+    id = fields.Integer(dump_only=True)
+    imagePaths = fields.String(validate=validate.Length(6, 12), required=True)
+    title = fields.String(validate=validate.Length(6, 12), required=True)
+    insertTime = fields.DateTime(dump_only=True)
+    pubtime = fields.DateTime(dump_only=True)
+    shortContent = fields.String(validate=validate.Length(6, 12), required=True)
+    source = fields.String(validate=validate.Length(6, 12), required=True)
+
+class LocalSchema(Schema):
+    """
+    地方
+    author:lyfy
+    :return:Id，imagePaths，insertTime，pubtime，shortContent,source,title
+    """
+    id = fields.Integer(dump_only=True)
+    imagePaths = fields.String(validate=validate.Length(6, 12), required=True)
+    title = fields.String(validate=validate.Length(6, 12), required=True)
+    insertTime = fields.DateTime(dump_only=True)
+    pubtime = fields.DateTime(dump_only=True)
+    shortContent = fields.String(validate=validate.Length(6, 12), required=True)
+    source = fields.String(validate=validate.Length(6, 12), required=True)
+
+class SgroupsSchema(Schema):
+    """
+    社会团体
+    author:lyfy
+    :return:Id,imagePaths,insertTime,pubtime,shortContent,source,title
+    """
+    id = fields.Integer(dump_only=True)
+    imagePaths = fields.String(validate=validate.Length(6, 12), required=True)
+    title = fields.String(validate=validate.Length(6, 12), required=True)
+    insertTime = fields.DateTime(dump_only=True)
+    pubtime = fields.DateTime(dump_only=True)
+    shortContent = fields.String(validate=validate.Length(6, 12), required=True)
+    source = fields.String(validate=validate.Length(6, 12), required=True)
+
+class BaseCitySchema(Schema):
+    """
+    基地
+    author:lyfy
+    :return:Id，imagePaths，insertTime，pubtime，shortContent，source，title
+    """
+    id = fields.Integer(dump_only=True)
+    imagePaths = fields.String(validate=validate.Length(6, 12), required=True)
+    title = fields.String(validate=validate.Length(6, 12), required=True)
+    insertTime = fields.DateTime(dump_only=True)
+    pubtime = fields.DateTime(dump_only=True)
+    shortContent = fields.String(validate=validate.Length(6, 12), required=True)
+    source = fields.String(validate=validate.Length(6, 12), required=True)
+
+class LpolicySchema(Schema):
+    """
+    最新政策
+    author:lyfy
+    :return:Id，pubTime，shortContent，source，titile
+    """
     id = fields.Integer(dump_only=True)
     title = fields.String(validate=validate.Length(6, 12), required=True)
-    content = fields.String(validate=validate.Length(6, 12), required=True)
-    updated_at = fields.DateTime(dump_only=True)
-    created_at = fields.DateTime(dump_only=True)
+    pubtime = fields.DateTime(dump_only=True)
+    shortContent = fields.String(validate=validate.Length(6, 12), required=True)
+    source = fields.String(validate=validate.Length(6, 12), required=True)
 
-class Indexpas(Schema):
-    title = fields.String(dump_only=True)
-    source = fields.String(dump_only=True)
-    updated_at = fields.DateTime(dump_only=True)
-    created_at = fields.DateTime(dump_only=True)
+class PanalysisSchema(Schema):
+    """
+    政策分析
+    author:lyfy
+    :return:businessId，title
+    """
+    businessId = fields.Integer(dump_only=True)
+    title = fields.String(validate=validate.Length(6, 12), required=True)
 
-class Indexactive(Schema):
+
+class AtrackingSchema(Schema):
+    """
+    活动跟踪
+    author:lyfy
+    :return:Category，id，picPath，publishTime，source，title
+    """
     id = fields.Integer(dump_only=True)
-    title = fields.String(dump_only=True)
-    source = fields.String(dump_only=True)
-    updated_at = fields.DateTime(dump_only=True)
-    created_at = fields.DateTime(dump_only=True)
+    title = fields.String(validate=validate.Length(6, 12), required=True)
+    picPath = fields.String(validate=validate.Length(6, 12), required=True)
+    publishTime = fields.DateTime(dump_only=True)
+    source = fields.String(validate=validate.Length(6, 12), required=True)
+    Category = fields.String(validate=validate.Length(6, 12), required=True)
 
-class Indexservice(Schema):
-    title = fields.String(dump_only=True)
-    source = fields.String(dump_only=True)
-    updated_at = fields.DateTime(dump_only=True)
-    created_at = fields.DateTime(dump_only=True)
-
-class Indexcity(Schema):
-    id = fields.Integer(dump_only=True)
-    title = fields.String(dump_only=True)
-    image = fields.String(dump_only=True)
-    source = fields.String(dump_only=True)
-    updated_at = fields.DateTime(dump_only=True)
-    created_at = fields.DateTime(dump_only=True)
-
-class Indexnews(Schema):
-    id = fields.Integer(dump_only=True)
-    title = fields.String(dump_only=True)
-    image = fields.String(dump_only=True)
-    source = fields.String(dump_only=True)
-    updated_at = fields.DateTime(dump_only=True)
-    created_at = fields.DateTime(dump_only=True)
-
-class Indexhot(Schema):
-    id = fields.Integer(dump_only=True)
-    title = fields.String(dump_only=True)
-    image = fields.String(dump_only=True)
-    source = fields.String(dump_only=True)
-    updated_at = fields.DateTime(dump_only=True)
-    created_at = fields.DateTime(dump_only=True)
-
-class Indexteam(Schema):
-    id = fields.Integer(dump_only=True)
-    title = fields.String(dump_only=True)
-    image = fields.String(dump_only=True)
-    source = fields.String(dump_only=True)
-    updated_at = fields.DateTime(dump_only=True)
-    created_at = fields.DateTime(dump_only=True)
-
-class Indexbasecity(Schema):
-    id = fields.Integer(dump_only=True)
-    title = fields.String(dump_only=True)
-    image = fields.String(dump_only=True)
-    source = fields.String(dump_only=True)
-    updated_at = fields.DateTime(dump_only=True)
-    created_at = fields.DateTime(dump_only=True)
+class ScolumnSchema(Schema):
+    """
+    专题专栏
+    author:lyfy
+    :return:Id，title
+    """
+    id = fields.String(validate=validate.Length(6, 12), required=True)
+    title = fields.String(validate=validate.Length(6, 12), required=True)
